9a78fc998c5219c6ead0ddca77af890d5dee267b
Mahlum Innovations LLC <colter@mahluminnovations.com>
2024-12-18 00:41:21 -0700

    remove all in font import

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -3,7 +3,7 @@ import ChatInterface from './components/ChatInterface';
 import FileUpload from './components/FileUpload';
 import { ThemeProvider, ThemeContext } from './ThemeContext';
 import { useMsal } from '@azure/msal-react';
-import '@fortawesome/fontawesome-pro/css/all.min.css';
+import '@fortawesome/fontawesome-pro/css/all.css';
 
 export default function App() {
     const { instance } = useMsal();
